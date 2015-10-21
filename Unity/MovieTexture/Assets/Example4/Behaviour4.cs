6b3c53322ed2de3aef0f0a892e4b2ce0462a1de1
Mk-10:cellfusion <makoto@cellfusion.jp>
2015-10-21 20:09:12 +0900

    uGUI の RawImage サンプル追加

---
+++ Unity/MovieTexture/Assets/Example4/Behaviour4.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Behaviour4 : MonoBehaviour {
+	
+	public RawImage _rawImage;
+	GLMovieTextureObject _mto;
+	
+	void Start(){
+		LoadMovieTexture("Movie2.m4v");
+	}
+	
+	void OnDestroy(){
+		if( _mto != null ){
+			Destroy(_mto);
+		}
+	}
+	
+	void LoadMovieTexture(string moviePath){
+		if( _rawImage == null || moviePath == null ){ return; }
+		if( _mto != null ){
+			Destroy(_mto);
+			_mto = null;	
+		}
+		
+		Texture2D texture = new Texture2D(1,1,TextureFormat.ARGB32, false);
+		_mto = ScriptableObject.CreateInstance<GLMovieTextureObject>();
+		_mto.Load(texture, moviePath);
+		_mto.SetLoop(true);
+		_mto.Play();
+		
+		_rawImage.texture = texture;
+	}
+}
